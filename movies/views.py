429220fe44c024fd6fb5fab3d4360ffcbbd34f71
zxc026 <mirslv971@gmail.com>
2026-04-25 10:42:33 +0900

    task6

--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,51 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.db.models import Avg
 from .models import Movie, Director, Review
 from movies import forms
 
+
 def index(request):
-    movies = Movie.objects.select_related("director").prefetch_related("genres").all()
-    return render(request, "movies/index.html", {"movies": movies})
+    # Сортировка
+    sort_by = request.GET.get('sort', '-year')
+
+    movies = Movie.objects.select_related("director").prefetch_related("genres").annotate(
+        avg_rating=Avg('reviews__rating')
+    )
+
+    if sort_by == 'title':
+        movies = movies.order_by('title')
+    elif sort_by == '-title':
+        movies = movies.order_by('-title')
+    elif sort_by == 'year':
+        movies = movies.order_by('year')
+    elif sort_by == '-year':
+        movies = movies.order_by('-year')
+    elif sort_by == 'avg_rating':
+        movies = movies.order_by('-avg_rating')
+    else:
+        movies = movies.order_by(sort_by)
+
+    # Пагинация
+    paginator = Paginator(movies, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "movies/index.html", {
+        "page_obj": page_obj,
+        "current_sort": sort_by
+    })
 
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
     reviews = movie.reviews.all()
-    return render(request, "movies/movie_detail.html", {"movie": movie, "review": reviews, "form": forms.ReviewForm()})
+    return render(request, "movies/movie_detail.html", {
+        "movie": movie,
+        "reviews": reviews,
+        "form": forms.ReviewForm()
+    })
+
 
 def add_reviews(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
